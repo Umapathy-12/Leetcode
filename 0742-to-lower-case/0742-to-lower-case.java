f22854a925b890a2dcf3e3f7629dbe597fd80018
umapathy <uumapathy411@gmail.com>
2025-08-05 23:22:08 +0530

    Time: 1 ms (21.75%), Space: 41.2 MB (98.7%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String s1=s.toLowerCase();
+        return s1;
+         }
+         public static void main(String[]args){
+            Scanner scan=new Scanner(System.in);
+            String a=scan.nextLine();
+            Solution b=new Solution();
+            System.out.println(b.toLowerCase(a));
+         }
+}
